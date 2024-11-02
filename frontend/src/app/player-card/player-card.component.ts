2c6a321d4552ece73ba164981fa729dbb485329c
Roman Bolshakov <rombolshak@gmail.com>
2024-11-02 20:07:04 +0300

    cost outline

--- frontend/src/app/player-card/player-card.component.ts
+++ frontend/src/app/player-card/player-card.component.ts
@@ -7,26 +7,7 @@ import {NgOptimizedImage} from '@angular/common';
   imports: [
     NgOptimizedImage
   ],
-  template: `
-    <img ngSrc="/assets/images/AHLCG-Asset-K.png" fill class="-z-10" />
-    <img src="/assets/images/09045.jpg" class="-z-20 absolute top-7"  />
-    <div class="text-white w-6 text-center text-3xl absolute top-2.5 left-6 font-teutonic drop-shadow-[2px_2px_2px_rgba(0,0,0,1)]">1</div>
-    <p class="text-center w-full absolute top-3 font-conkordia text-2xl">
-      Research Notes
-    </p>
-    <p class="absolute text-[11px] bold uppercase top-[3.85rem] left-5 font-arno">Актив</p>
-    <div class="absolute top-20">
-      <img src="/assets/images/AHLCG-SkillBox-K.png" class="relative">
-      <img src="/assets/images/AHLCG-SkillIcon-I.png" class="absolute top-2 left-3">
-    </div>
-
-    <div class="absolute top-80 w-full leading-4 px-4 font-arno" style="font-size: 1.15vw;">
-      <p class="text-center mb-1 italic font-arno-bold"><span>Item. </span><span>Tome. </span><span>Science. </span></p>
-      <p class="mb-1"><span class="font-ah-symbol">r </span> После того как свойство карты игрока поместило в вашу локацию хотя бы 1 вашу улику: Положите на эту карту столько же ресурсов как подтверждения.</p>
-      <p><span class="font-ah-symbol">n</span>: Пройдите проверку <span class="font-ah-symbol">i</span>(0). За каждую единицу, на которую превысили сложность, можете потратить 1 подтверждение, чтобы найти 1 улику в вашей локации</p>
-    </div>
-    <img src="/assets/images/AHLCG-Slot-1 Hand.png" class="absolute bottom-3 right-3" />
-  `,
+  templateUrl: './player-card.component.html',
   styles: `:host { @apply block relative; width: 375px; height: 525px; }`,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
