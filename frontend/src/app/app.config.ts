3a8682e5a6fe546c35b699584a1d775afecdeb63
Roman Bolshakov <rombolshak@gmail.com>
2025-02-10 14:03:54 +0300

    chore: bugsnug integration

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,16 +1,31 @@
 import {
   ApplicationConfig,
+  ErrorHandler,
   provideExperimentalZonelessChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { BugsnagErrorHandler } from '@bugsnag/plugin-angular';
+import BugsnagPerformance from '@bugsnag/browser-performance';
+import Bugsnag from '@bugsnag/js';
+
+Bugsnag.start({ apiKey: 'c83772d54325525fdd6f016c4c49f3df' });
+BugsnagPerformance.start({ apiKey: 'c83772d54325525fdd6f016c4c49f3df' });
+
+function errorHandlerFactory() {
+  return new BugsnagErrorHandler();
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(),
     provideRouter(routes),
     provideAnimationsAsync(),
+    {
+      provide: ErrorHandler,
+      useFactory: errorHandlerFactory,
+    },
   ],
 };
