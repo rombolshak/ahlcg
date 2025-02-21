39de350108e988a97169908dfe2f8b496ca82d3d
Roman Bolshakov <rombolshak@gmail.com>
2025-02-21 11:22:53 +0300

    feat: general template for enemies

--- frontend/src/app/pages/game-view/components/play-area/location/location.component.ts
+++ frontend/src/app/pages/game-view/components/play-area/location/location.component.ts
@@ -10,10 +10,17 @@ import { Location } from 'shared/domain/location.model';
 import { LocationHeaderComponent } from './location-header/location-header.component';
 import { InvestigatorWithState } from 'shared/domain/player-card.model';
 import { EntityAvatarComponent } from '../../entity-avatar/entity-avatar.component';
+import { SetInfo } from '../../../../../shared/domain/card-info.model';
+import { VitalsBarComponent } from '../../../../../shared/ui/components/vitals-bar/vitals-bar.component';
 
 @Component({
   selector: 'ah-location',
-  imports: [NgOptimizedImage, LocationHeaderComponent, EntityAvatarComponent],
+  imports: [
+    NgOptimizedImage,
+    LocationHeaderComponent,
+    EntityAvatarComponent,
+    VitalsBarComponent,
+  ],
   templateUrl: './location.component.html',
   host: {
     class: 'flex flex-col rounded-3xl outline outline-2 outline-zinc-400',
@@ -24,4 +31,8 @@ export class LocationComponent {
   imageService = inject(ImagesUrlService);
   readonly location = input.required<Location>();
   readonly investigators = input.required<InvestigatorWithState[]>();
+  readonly enemyImage: SetInfo = {
+    set: '01',
+    index: '119',
+  };
 }
