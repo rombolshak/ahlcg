473ecad01e25d38d3f4db7591349f2d14077e44a
Roman Bolshakov <rombolshak@gmail.com>
2025-03-03 23:01:44 +0300

    feat: enemy details display (#57)

--- frontend/src/app/pages/game-view/components/enemy-avatar/enemy-avatar.component.ts
+++ frontend/src/app/pages/game-view/components/enemy-avatar/enemy-avatar.component.ts
@@ -7,18 +7,31 @@ import {
 import { VitalsBarComponent } from 'shared/ui/components/vitals-bar/vitals-bar.component';
 import { NgOptimizedImage } from '@angular/common';
 import { EnemyWithState } from 'shared/domain/enemy.model';
-import { ImagesUrlService } from 'shared/services/images-url.service';
+import {
+  CreateOverlay,
+  ImagesUrlService,
+} from 'shared/services/images-url.service';
+import { CardDetailsTextComponent } from '../card-details-text/card-details-text.component';
+import { SkillType } from '../../../../shared/domain/player-card.model';
+import { EnemyPopoverComponent } from './enemy-popover/enemy-popover.component';
 
 @Component({
   selector: 'ah-enemy-avatar',
-  imports: [VitalsBarComponent, NgOptimizedImage],
+  imports: [
+    VitalsBarComponent,
+    NgOptimizedImage,
+    CardDetailsTextComponent,
+    EnemyPopoverComponent,
+  ],
   templateUrl: './enemy-avatar.component.html',
   host: {
-    class: 'bg-zinc-900/70',
+    class: 'relative bg-zinc-900/70',
   },
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EnemyAvatarComponent {
   readonly enemy = input.required<EnemyWithState>();
   readonly imageService = inject(ImagesUrlService);
+  protected readonly CreateOverlay = CreateOverlay;
+  protected readonly SkillType = SkillType;
 }
---
+++ frontend/src/app/pages/game-view/components/enemy-avatar/enemy-popover/enemy-popover.component.spec.ts
@@ -0,0 +1,26 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EnemyPopoverComponent } from './enemy-popover.component';
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { testEnemy } from '../../../../../shared/domain/test/test-enemies';
+
+describe('EnemyPopoverComponent', () => {
+  let component: EnemyPopoverComponent;
+  let fixture: ComponentFixture<EnemyPopoverComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [EnemyPopoverComponent],
+      providers: [provideExperimentalZonelessChangeDetection()],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(EnemyPopoverComponent);
+    component = fixture.componentInstance;
+    fixture.componentRef.setInput('enemy', testEnemy);
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
