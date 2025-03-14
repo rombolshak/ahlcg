cfdf4441cb6c424fd84b96d08f7553efb5081bef
Roman Bolshakov <rombolshak@gmail.com>
2025-03-14 16:20:26 +0300

    feat: locations grid

--- frontend/src/app/pages/game-view/central-view/play-area/play-area.component.ts
+++ frontend/src/app/pages/game-view/central-view/play-area/play-area.component.ts
@@ -17,31 +17,15 @@ import { testEnemy } from 'shared/domain/test/test-enemies';
 @Component({
   selector: 'ah-play-area',
   imports: [LocationComponent],
-  template: `
-    <div
-      #playArea
-      class="grid grid-cols-[repeat(9,44rem)] grid-rows-[repeat(9,27rem)] w-max"
-    >
-      <ah-location
-        class="relative col-start-1 row-start-1 rounded"
-        [location]="location"
-        [investigators]="investigators"
-      />
-    </div>
-  `,
-  // eslint-disable-next-line @angular-eslint/component-max-inline-declarations
-  styles: `
-    :host {
-      display: block;
-      width: 100%;
-      height: 100%;
-      overflow: hidden;
-    }
-  `,
+  templateUrl: './play-area.component.html',
+  host: {
+    class: 'block w-full h-full overflow-hidden',
+  },
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PlayAreaComponent implements AfterViewInit {
   location: Location = testLocation;
+
   enemy: Enemy = testEnemy;
   investigator1 = {
     ...InvestigatorS,
@@ -89,6 +73,31 @@ export class PlayAreaComponent implements AfterViewInit {
     this.investigator1,
   ];
 
+  worldMap = {
+    width: 6,
+    height: 6,
+    locations: [
+      {
+        x: 2,
+        y: 3,
+        location: testLocation,
+        investigators: this.investigators,
+      },
+      {
+        x: 4,
+        y: 2,
+        location: testLocation,
+        investigators: [],
+      },
+      {
+        x: 4,
+        y: 4,
+        location: testLocation,
+        investigators: [],
+      },
+    ],
+  };
+
   public ngAfterViewInit() {
     this.zoomArea = Panzoom(this.playArea()?.nativeElement as HTMLElement, {
       contain: 'outside',
@@ -98,14 +107,14 @@ export class PlayAreaComponent implements AfterViewInit {
 
     const parent = (this.playArea()?.nativeElement as HTMLElement)
       .parentNode as HTMLElement;
-    const locX = 0;
-    const locY = 0;
+    const locX = 1;
+    const locY = 2;
     const cx =
-      -locX * this.locationWidth +
+      -locX * (this.locationWidth - this.gapWidth / 2) +
       parent.offsetWidth / 2 -
       this.locationWidth / 2;
     const cy =
-      -locY * this.locationHeight +
+      -locY * (this.locationHeight - this.gapHeight) +
       parent.offsetHeight / 2 -
       this.locationHeight / 2;
 
@@ -121,7 +130,9 @@ export class PlayAreaComponent implements AfterViewInit {
   }
 
   locationWidth = 44 * 16;
-  locationHeight = 31 * 16;
+  locationHeight = 27 * 16;
+  gapWidth = 22 * 16;
+  gapHeight = 9 * 16;
 
   private readonly playArea = viewChild<ElementRef>('playArea');
 
