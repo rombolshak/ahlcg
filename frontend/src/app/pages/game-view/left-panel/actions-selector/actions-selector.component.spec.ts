4d9a1adf6ac35245c3a25ee7164806ce57790c60
Roman Bolshakov <rombolshak@gmail.com>
2025-03-14 14:02:15 +0300

    feat: actions list

---
+++ frontend/src/app/pages/game-view/left-panel/actions-selector/actions-selector.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ActionsSelectorComponent } from './actions-selector.component';
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
+
+describe('ActionsSelectorComponent', () => {
+  let component: ActionsSelectorComponent;
+  let fixture: ComponentFixture<ActionsSelectorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ActionsSelectorComponent],
+      providers: [provideExperimentalZonelessChangeDetection()],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ActionsSelectorComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- frontend/src/app/pages/game-view/left-panel/left-panel.component.ts
+++ frontend/src/app/pages/game-view/left-panel/left-panel.component.ts
@@ -2,10 +2,15 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { ThreatAreaComponent } from './threat-area/threat-area.component';
 import { InvestigatorComponent } from './investigator/investigator.component';
 import { InvestigatorWithState } from 'shared/domain/investigator.model';
+import { ActionsSelectorComponent } from './actions-selector/actions-selector.component';
 
 @Component({
   selector: 'ah-left-panel',
-  imports: [ThreatAreaComponent, InvestigatorComponent],
+  imports: [
+    ThreatAreaComponent,
+    InvestigatorComponent,
+    ActionsSelectorComponent,
+  ],
   templateUrl: './left-panel.component.html',
   host: {
     class: 'flex flex-col gap-4',
