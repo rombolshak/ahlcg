a139f05fc98128b7813a5bd48161ae9c72940a45
Roman Bolshakov <rombolshak@gmail.com>
2025-06-02 19:52:04 +0300

    feat: extended state validation (#153)

---
+++ frontend/src/app/shared/domain/game-state.spec.ts
@@ -0,0 +1,300 @@
+﻿/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { gameState } from './game-state';
+import { testGameState } from './test/test-game-state';
+import { ArkErrors } from 'arktype';
+import { produce } from 'immer';
+import {
+  actId,
+  agendaId,
+  assetId,
+  enemyId,
+  investigatorId,
+  locationId,
+} from './entities/id.model';
+import { Investigator } from './entities/investigator.model';
+
+describe('GameState', () => {
+  it('should accept valid state', () => {
+    const state = gameState(testGameState);
+
+    expect(state).not.toBeInstanceOf(ArkErrors);
+  });
+
+  it('should show error for connection same from and to', () => {
+    const testState = produce(testGameState, (state) => {
+      const connection = state.scenarioMap.connections[0];
+
+      expect(connection).toBeTruthy();
+      connection!.from = connection!.to;
+    });
+
+    const validated = gameState(testState);
+
+    expect(validated).toBeInstanceOf(ArkErrors);
+    expect((validated as ArkErrors).summary).toContain(
+      'Illegal connection to the same location',
+    );
+  });
+
+  it('should show error for invalid connection from', () => {
+    const testState = produce(testGameState, (state) => {
+      const connection = state.scenarioMap.connections[0];
+
+      expect(connection).toBeTruthy();
+      connection!.from = locationId.assert('1234');
+    });
+
+    const validated = gameState(testState);
+
+    expect(validated).toBeInstanceOf(ArkErrors);
+    expect((validated as ArkErrors).summary).toContain(
+      '1234 is not specified in map',
+    );
+  });
+
+  it('should show error for invalid connection to', () => {
+    const testState = produce(testGameState, (state) => {
+      const connection = state.scenarioMap.connections[0];
+
+      expect(connection).toBeTruthy();
+      connection!.to = locationId.assert('1234');
+    });
+
+    const validated = gameState(testState);
+
+    expect(validated).toBeInstanceOf(ArkErrors);
+    expect((validated as ArkErrors).summary).toContain(
+      '1234 is not specified in map',
+    );
+  });
+
+  it('should show error for non existent investigator in location', () => {
+    const testState = produce(testGameState, (state) => {
+      const place = state.scenarioMap.places[0];
+
+      expect(place).toBeTruthy();
+      place!.investigators = [investigatorId.assert('1234')];
+    });
+
+    const validated = gameState(testState);
+
+    expect(validated).toBeInstanceOf(ArkErrors);
+    expect((validated as ArkErrors).summary).toContain('does not exists');
+  });
+
+  it('should show error for invalid investigator in location', () => {
+    const testState = produce(testGameState, (state) => {
+      const place = state.scenarioMap.places[0];
+
+      expect(place).toBeTruthy();
+      place!.investigators = [investigatorId.assert('2129')];
+    });
+
+    const validated = gameState(testState);
+
+    expect(validated).toBeInstanceOf(ArkErrors);
+    expect((validated as ArkErrors).summary).toContain('must be investigator');
+  });
+
+  it('should show error for non existent location in place', () => {
+    const testState = produce(testGameState, (state) => {
+      const place = state.scenarioMap.places[0];
+
+      expect(place).toBeTruthy();
+      place!.location = locationId.assert('1234');
+    });
+
+    const validated = gameState(testState);
+
+    expect(validated).toBeInstanceOf(ArkErrors);
+    expect((validated as ArkErrors).summary).toContain('does not exists');
+  });
+
+  it('should show error for invalid location in place', () => {
+    const testState = produce(testGameState, (state) => {
+      const place = state.scenarioMap.places[0];
+
+      expect(place).toBeTruthy();
+      place!.location = locationId.assert('1003');
+    });
+
+    const validated = gameState(testState);
+
+    expect(validated).toBeInstanceOf(ArkErrors);
+    expect((validated as ArkErrors).summary).toContain('must be location');
+  });
+
+  it('should show error for non existent asset of investigator', () => {
+    const testState = produce(testGameState, (state) => {
+      const investigator = state.gameEntities['1003'] as Investigator;
+
+      expect(investigator).toBeTruthy();
+      investigator.controlledAssets = [assetId.assert('1234')];
+    });
+
+    const validated = gameState(testState);
+
+    expect(validated).toBeInstanceOf(ArkErrors);
+    expect((validated as ArkErrors).summary).toContain('does not exists');
+  });
+
+  it('should show error for invalid asset of investigator', () => {
+    const testState = produce(testGameState, (state) => {
+      const investigator = state.gameEntities['1003'] as Investigator;
+
+      expect(investigator).toBeTruthy();
+      investigator.controlledAssets = [assetId.assert('7')];
+    });
+
+    const validated = gameState(testState);
+
+    expect(validated).toBeInstanceOf(ArkErrors);
+    expect((validated as ArkErrors).summary).toContain('must be asset');
+  });
+
+  it('should show error for non existent hand of investigator', () => {
+    const testState = produce(testGameState, (state) => {
+      const investigator = state.gameEntities['1003'] as Investigator;
+
+      expect(investigator).toBeTruthy();
+      investigator.hand = [assetId.assert('1234')];
+    });
+
+    const validated = gameState(testState);
+
+    expect(validated).toBeInstanceOf(ArkErrors);
+    expect((validated as ArkErrors).summary).toContain('does not exists');
+  });
+
+  it('should show error for invalid hand of investigator', () => {
+    const testState = produce(testGameState, (state) => {
+      const investigator = state.gameEntities['1003'] as Investigator;
+
+      expect(investigator).toBeTruthy();
+      investigator.hand = [assetId.assert('2129')];
+    });
+
+    const validated = gameState(testState);
+
+    expect(validated).toBeInstanceOf(ArkErrors);
+    expect((validated as ArkErrors).summary).toContain(
+      'must be asset or skill or event',
+    );
+  });
+
+  it('should show error for non existent enemy of investigator', () => {
+    const testState = produce(testGameState, (state) => {
+      const investigator = state.gameEntities['1003'] as Investigator;
+
+      expect(investigator).toBeTruthy();
+      investigator.threatArea = [enemyId.assert('1234')];
+    });
+
+    const validated = gameState(testState);
+
+    expect(validated).toBeInstanceOf(ArkErrors);
+    expect((validated as ArkErrors).summary).toContain('does not exists');
+  });
+
+  it('should show error for invalid enemy of investigator', () => {
+    const testState = produce(testGameState, (state) => {
+      const investigator = state.gameEntities['1003'] as Investigator;
+
+      expect(investigator).toBeTruthy();
+      investigator.threatArea = [enemyId.assert('7')];
+    });
+
+    const validated = gameState(testState);
+
+    expect(validated).toBeInstanceOf(ArkErrors);
+    expect((validated as ArkErrors).summary).toContain('must be enemy');
+  });
+
+  it('should show error for non existent investigator', () => {
+    const testState = produce(testGameState, (state) => {
+      state.investigators = [investigatorId.assert('1234')];
+    });
+
+    const validated = gameState(testState);
+
+    expect(validated).toBeInstanceOf(ArkErrors);
+    expect((validated as ArkErrors).summary).toContain('does not exists');
+  });
+
+  it('should show error for invalid investigator', () => {
+    const testState = produce(testGameState, (state) => {
+      state.investigators = [investigatorId.assert('2129')];
+    });
+
+    const validated = gameState(testState);
+
+    expect(validated).toBeInstanceOf(ArkErrors);
+    expect((validated as ArkErrors).summary).toContain('must be investigator');
+  });
+
+  it('should show error for non existent current investigator', () => {
+    const testState = produce(testGameState, (state) => {
+      state.currentInvestigator = investigatorId.assert('1234');
+    });
+
+    const validated = gameState(testState);
+
+    expect(validated).toBeInstanceOf(ArkErrors);
+    expect((validated as ArkErrors).summary).toContain('does not exists');
+  });
+
+  it('should show error for invalid current investigator', () => {
+    const testState = produce(testGameState, (state) => {
+      state.currentInvestigator = investigatorId.assert('2129');
+    });
+
+    const validated = gameState(testState);
+
+    expect(validated).toBeInstanceOf(ArkErrors);
+    expect((validated as ArkErrors).summary).toContain('must be investigator');
+  });
+
+  it('should show error for non existent act', () => {
+    const testState = produce(testGameState, (state) => {
+      state.acts = [actId.assert('1234')];
+    });
+
+    const validated = gameState(testState);
+
+    expect(validated).toBeInstanceOf(ArkErrors);
+    expect((validated as ArkErrors).summary).toContain('does not exists');
+  });
+
+  it('should show error for invalid act', () => {
+    const testState = produce(testGameState, (state) => {
+      state.acts = [actId.assert('1003')];
+    });
+
+    const validated = gameState(testState);
+
+    expect(validated).toBeInstanceOf(ArkErrors);
+    expect((validated as ArkErrors).summary).toContain('must be act');
+  });
+
+  it('should show error for non existent agenda', () => {
+    const testState = produce(testGameState, (state) => {
+      state.agendas = [agendaId.assert('1234')];
+    });
+
+    const validated = gameState(testState);
+
+    expect(validated).toBeInstanceOf(ArkErrors);
+    expect((validated as ArkErrors).summary).toContain('does not exists');
+  });
+
+  it('should show error for invalid agenda', () => {
+    const testState = produce(testGameState, (state) => {
+      state.agendas = [agendaId.assert('1003')];
+    });
+
+    const validated = gameState(testState);
+
+    expect(validated).toBeInstanceOf(ArkErrors);
+    expect((validated as ArkErrors).summary).toContain('must be agenda');
+  });
+});
--- frontend/src/app/shared/domain/game-state.ts
+++ frontend/src/app/shared/domain/game-state.ts
@@ -1,8 +1,44 @@
-﻿import { type } from 'arktype';
+﻿import { Traversal, type } from 'arktype';
 import { gameMap } from './game-map.model';
-import { actId, agendaId, investigatorId } from './entities/id.model';
+import { actId, agendaId, EntityId, investigatorId } from './entities/id.model';
 import { action } from './action.model';
-import { gameEntity } from './game-entity';
+import { GameEntity, gameEntity } from './game-entity';
+import { CardType } from './entities/card.model';
+import { Investigator } from './entities/investigator.model';
+
+function validateEntity(
+  id: EntityId | undefined,
+  gameEntities: Record<EntityId, GameEntity>,
+  ctx: Traversal,
+  path: readonly string[],
+  entityType: CardType | CardType[],
+): boolean {
+  if (!id)
+    return ctx.reject({
+      path: path,
+      problem: `${entityType.toString()}Id is undefined.`,
+    });
+  if (!gameEntities[id])
+    return ctx.reject({
+      path: path,
+      problem: `${entityType.toString()} '${id}' does not exists in gameEntities.`,
+    });
+  if (entityType instanceof Array) {
+    if (!entityType.find((v) => v === gameEntities[id]?.type))
+      return ctx.reject({
+        path: path,
+        expected: entityType.join(' or '),
+        actual: gameEntities[id].type,
+      });
+  } else if (gameEntities[id].type !== entityType)
+    return ctx.reject({
+      path: path,
+      expected: entityType,
+      actual: gameEntities[id].type,
+    });
+
+  return true;
+}
 
 export const gameState = type({
   acts: actId.array().atLeastLength(1),
@@ -14,6 +50,154 @@ export const gameState = type({
   gameEntities: {
     '[string]': gameEntity,
   },
+}).narrow((state, ctx) => {
+  for (let i = 0; i < state.acts.length; i++) {
+    if (
+      !validateEntity(
+        state.acts[i],
+        state.gameEntities,
+        ctx,
+        ['acts', i.toString()],
+        'act',
+      )
+    )
+      return false;
+  }
+
+  for (let i = 0; i < state.agendas.length; i++) {
+    if (
+      !validateEntity(
+        state.agendas[i],
+        state.gameEntities,
+        ctx,
+        ['agendas', i.toString()],
+        'agenda',
+      )
+    )
+      return false;
+  }
+
+  if (
+    !validateEntity(
+      state.currentInvestigator,
+      state.gameEntities,
+      ctx,
+      ['currentInvestigator'],
+      'investigator',
+    )
+  )
+    return false;
+
+  for (let i = 0; i < state.investigators.length; i++) {
+    if (
+      !validateEntity(
+        state.investigators[i],
+        state.gameEntities,
+        ctx,
+        ['investigators', i.toString()],
+        'investigator',
+      )
+    )
+      return false;
+
+    const investigator = state.gameEntities[
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      state.investigators[i]!
+    ] as Investigator;
+    for (let e = 0; e < investigator.threatArea.length; e++) {
+      if (
+        !validateEntity(
+          investigator.threatArea[e],
+          state.gameEntities,
+          ctx,
+          ['gameEntities', investigator.id, 'threatArea', e.toString()],
+          'enemy',
+        )
+      )
+        return false;
+    }
+
+    for (let h = 0; h < investigator.hand.length; h++) {
+      if (
+        !validateEntity(
+          investigator.hand[h],
+          state.gameEntities,
+          ctx,
+          ['gameEntities', investigator.id, 'hand', h.toString()],
+          ['asset', 'skill', 'event'],
+        )
+      )
+        return false;
+    }
+
+    for (let e = 0; e < investigator.controlledAssets.length; e++) {
+      if (
+        !validateEntity(
+          investigator.controlledAssets[e],
+          state.gameEntities,
+          ctx,
+          ['gameEntities', investigator.id, 'controlledAssets', e.toString()],
+          'asset',
+        )
+      )
+        return false;
+    }
+  }
+
+  for (let i = 0; i < state.scenarioMap.places.length; i++) {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const place = state.scenarioMap.places[i]!;
+    if (
+      !validateEntity(
+        place.location,
+        state.gameEntities,
+        ctx,
+        ['scenarioMap', 'places', i.toString(), 'location'],
+        'location',
+      )
+    )
+      return false;
+    for (let e = 0; e < place.investigators.length; e++) {
+      if (
+        !validateEntity(
+          place.investigators[e],
+          state.gameEntities,
+          ctx,
+          [
+            'scenarioMap',
+            'places',
+            i.toString(),
+            'investigators',
+            e.toString(),
+          ],
+          'investigator',
+        )
+      )
+        return false;
+    }
+  }
+
+  for (let i = 0; i < state.scenarioMap.connections.length; i++) {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const connection = state.scenarioMap.connections[i]!;
+    if (!state.scenarioMap.places.find((p) => p.location === connection.from))
+      return ctx.reject({
+        path: ['scenarioMap', 'connections', i.toString(), 'from'],
+        problem: `place ${connection.from} is not specified in map`,
+      });
+    if (!state.scenarioMap.places.find((p) => p.location === connection.to))
+      return ctx.reject({
+        path: ['scenarioMap', 'connections', i.toString(), 'to'],
+        problem: `place ${connection.to} is not specified in map`,
+      });
+    if (connection.from === connection.to)
+      return ctx.reject({
+        path: ['scenarioMap', 'connections', i.toString()],
+        problem: `Illegal connection to the same location`,
+      });
+  }
+
+  return true;
 });
 
 export type GameState = typeof gameState.infer;
