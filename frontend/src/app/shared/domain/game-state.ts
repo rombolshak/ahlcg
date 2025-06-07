e9d5b08a2d95b9a837a87db8477334119baf3aa1
Roman Bolshakov <rombolshak@gmail.com>
2025-06-07 21:44:00 +0300

    chore: sonar issue 1 (#156)
    * feat: card info service
    
    * chore: lower congnitive complexity

--- frontend/src/app/shared/domain/game-state.ts
+++ frontend/src/app/shared/domain/game-state.ts
@@ -1,14 +1,25 @@
 ﻿import { Traversal, type } from 'arktype';
 import { gameMap } from './game-map.model';
-import { actId, agendaId, EntityId, investigatorId } from './entities/id.model';
+import {
+  ActId,
+  actId,
+  AgendaId,
+  agendaId,
+  EntityId,
+  InvestigatorId,
+  investigatorId,
+  LocationId,
+} from './entities/id.model';
 import { action } from './action.model';
 import { GameEntity, gameEntity } from './game-entity';
 import { CardType } from './entities/card.model';
 import { Investigator } from './entities/investigator.model';
 
+type GameEntities = Record<EntityId, GameEntity>;
+
 function validateEntity(
   id: EntityId | undefined,
-  gameEntities: Record<EntityId, GameEntity>,
+  gameEntities: GameEntities,
   ctx: Traversal,
   path: readonly string[],
   entityType: CardType | CardType[],
@@ -40,6 +51,241 @@ function validateEntity(
   return true;
 }
 
+function validateActs(
+  state: { acts: ActId[]; gameEntities: GameEntities },
+  ctx: Traversal,
+) {
+  return state.acts.reduce(
+    (result, act, index) =>
+      result &&
+      validateEntity(
+        act,
+        state.gameEntities,
+        ctx,
+        ['acts', index.toString()],
+        'act',
+      ),
+    true,
+  );
+}
+
+function validateAgendas(
+  state: { agendas: AgendaId[]; gameEntities: GameEntities },
+  ctx: Traversal,
+) {
+  return state.agendas.reduce(
+    (result, agendaId, index) =>
+      result &&
+      validateEntity(
+        agendaId,
+        state.gameEntities,
+        ctx,
+        ['agendas', index.toString()],
+        'agenda',
+      ),
+    true,
+  );
+}
+
+function validateCurrentInvestigator(
+  state: {
+    currentInvestigator: InvestigatorId;
+    gameEntities: GameEntities;
+  },
+  ctx: Traversal,
+) {
+  return validateEntity(
+    state.currentInvestigator,
+    state.gameEntities,
+    ctx,
+    ['currentInvestigator'],
+    'investigator',
+  );
+}
+
+function validateSingleInvestigator(
+  state: {
+    investigators: InvestigatorId[];
+    gameEntities: GameEntities;
+  },
+  i: number,
+  ctx: Traversal,
+) {
+  return validateEntity(
+    state.investigators[i],
+    state.gameEntities,
+    ctx,
+    ['investigators', i.toString()],
+    'investigator',
+  );
+}
+
+function validateThreatArea(
+  investigator: Investigator,
+  state: { gameEntities: GameEntities },
+  ctx: Traversal,
+) {
+  return investigator.threatArea.reduce(
+    (result, threat, index) =>
+      result &&
+      validateEntity(
+        threat,
+        state.gameEntities,
+        ctx,
+        ['gameEntities', investigator.id, 'threatArea', index.toString()],
+        'enemy',
+      ),
+    true,
+  );
+}
+
+function validateHand(
+  investigator: Investigator,
+  state: { gameEntities: GameEntities },
+  ctx: Traversal,
+) {
+  return investigator.hand.reduce(
+    (result, card, index) =>
+      result &&
+      validateEntity(
+        card,
+        state.gameEntities,
+        ctx,
+        ['gameEntities', investigator.id, 'hand', index.toString()],
+        ['asset', 'skill', 'event'],
+      ),
+    true,
+  );
+}
+
+function validateAssets(
+  investigator: Investigator,
+  state: { gameEntities: GameEntities },
+  ctx: Traversal,
+) {
+  return investigator.controlledAssets.reduce(
+    (result, asset, index) =>
+      result &&
+      validateEntity(
+        asset,
+        state.gameEntities,
+        ctx,
+        ['gameEntities', investigator.id, 'controlledAssets', index.toString()],
+        'asset',
+      ),
+    true,
+  );
+}
+
+function validateInvestigators(
+  state: { investigators: InvestigatorId[]; gameEntities: GameEntities },
+  ctx: Traversal,
+) {
+  return state.investigators.reduce((result, investigatorId, index) => {
+    if (!validateSingleInvestigator(state, index, ctx)) return false;
+    const investigator = state.gameEntities[investigatorId] as Investigator;
+    return (
+      result &&
+      validateThreatArea(investigator, state, ctx) &&
+      validateHand(investigator, state, ctx) &&
+      validateAssets(investigator, state, ctx)
+    );
+  }, true);
+}
+
+function validatePlaceLocation(
+  place: { location: LocationId },
+  state: {
+    gameEntities: GameEntities;
+  },
+  ctx: Traversal,
+  i: number,
+) {
+  return validateEntity(
+    place.location,
+    state.gameEntities,
+    ctx,
+    ['scenarioMap', 'places', i.toString(), 'location'],
+    'location',
+  );
+}
+
+function validatePlaceInvestigator(
+  place: { investigators: InvestigatorId[] },
+  state: {
+    gameEntities: GameEntities;
+  },
+  ctx: Traversal,
+  i: number,
+) {
+  return place.investigators.reduce(
+    (result, investigatorId, index) =>
+      result &&
+      validateEntity(
+        investigatorId,
+        state.gameEntities,
+        ctx,
+        [
+          'scenarioMap',
+          'places',
+          i.toString(),
+          'investigators',
+          index.toString(),
+        ],
+        'investigator',
+      ),
+    true,
+  );
+}
+
+function validatePlaces(
+  state: {
+    scenarioMap: {
+      places: { location: LocationId; investigators: InvestigatorId[] }[];
+    };
+    gameEntities: GameEntities;
+  },
+  ctx: Traversal,
+) {
+  return state.scenarioMap.places.reduce((result, place, index) => {
+    return (
+      result &&
+      validatePlaceLocation(place, state, ctx, index) &&
+      validatePlaceInvestigator(place, state, ctx, index)
+    );
+  }, true);
+}
+
+function validateConnections(
+  state: {
+    scenarioMap: {
+      connections: { from: LocationId; to: LocationId }[];
+      places: { location: LocationId }[];
+    };
+  },
+  ctx: Traversal,
+) {
+  return state.scenarioMap.connections.reduce((result, connection, index) => {
+    if (!result) return result;
+    if (!state.scenarioMap.places.find((p) => p.location === connection.from))
+      return ctx.reject({
+        path: ['scenarioMap', 'connections', index.toString(), 'from'],
+        problem: `place ${connection.from} is not specified in map`,
+      });
+    if (!state.scenarioMap.places.find((p) => p.location === connection.to))
+      return ctx.reject({
+        path: ['scenarioMap', 'connections', index.toString(), 'to'],
+        problem: `place ${connection.to} is not specified in map`,
+      });
+    if (connection.from === connection.to)
+      return ctx.reject({
+        path: ['scenarioMap', 'connections', index.toString()],
+        problem: `Illegal connection to the same location`,
+      });
+    return true;
+  }, true);
+}
+
 export const gameState = type({
   acts: actId.array().atLeastLength(1),
   agendas: agendaId.array().atLeastLength(1),
@@ -50,154 +296,15 @@ export const gameState = type({
   gameEntities: {
     '[string]': gameEntity,
   },
-}).narrow((state, ctx) => {
-  for (let i = 0; i < state.acts.length; i++) {
-    if (
-      !validateEntity(
-        state.acts[i],
-        state.gameEntities,
-        ctx,
-        ['acts', i.toString()],
-        'act',
-      )
-    )
-      return false;
-  }
-
-  for (let i = 0; i < state.agendas.length; i++) {
-    if (
-      !validateEntity(
-        state.agendas[i],
-        state.gameEntities,
-        ctx,
-        ['agendas', i.toString()],
-        'agenda',
-      )
-    )
-      return false;
-  }
-
-  if (
-    !validateEntity(
-      state.currentInvestigator,
-      state.gameEntities,
-      ctx,
-      ['currentInvestigator'],
-      'investigator',
-    )
-  )
-    return false;
-
-  for (let i = 0; i < state.investigators.length; i++) {
-    if (
-      !validateEntity(
-        state.investigators[i],
-        state.gameEntities,
-        ctx,
-        ['investigators', i.toString()],
-        'investigator',
-      )
-    )
-      return false;
-
-    const investigator = state.gameEntities[
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      state.investigators[i]!
-    ] as Investigator;
-    for (let e = 0; e < investigator.threatArea.length; e++) {
-      if (
-        !validateEntity(
-          investigator.threatArea[e],
-          state.gameEntities,
-          ctx,
-          ['gameEntities', investigator.id, 'threatArea', e.toString()],
-          'enemy',
-        )
-      )
-        return false;
-    }
-
-    for (let h = 0; h < investigator.hand.length; h++) {
-      if (
-        !validateEntity(
-          investigator.hand[h],
-          state.gameEntities,
-          ctx,
-          ['gameEntities', investigator.id, 'hand', h.toString()],
-          ['asset', 'skill', 'event'],
-        )
-      )
-        return false;
-    }
-
-    for (let e = 0; e < investigator.controlledAssets.length; e++) {
-      if (
-        !validateEntity(
-          investigator.controlledAssets[e],
-          state.gameEntities,
-          ctx,
-          ['gameEntities', investigator.id, 'controlledAssets', e.toString()],
-          'asset',
-        )
-      )
-        return false;
-    }
-  }
-
-  for (let i = 0; i < state.scenarioMap.places.length; i++) {
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const place = state.scenarioMap.places[i]!;
-    if (
-      !validateEntity(
-        place.location,
-        state.gameEntities,
-        ctx,
-        ['scenarioMap', 'places', i.toString(), 'location'],
-        'location',
-      )
-    )
-      return false;
-    for (let e = 0; e < place.investigators.length; e++) {
-      if (
-        !validateEntity(
-          place.investigators[e],
-          state.gameEntities,
-          ctx,
-          [
-            'scenarioMap',
-            'places',
-            i.toString(),
-            'investigators',
-            e.toString(),
-          ],
-          'investigator',
-        )
-      )
-        return false;
-    }
-  }
-
-  for (let i = 0; i < state.scenarioMap.connections.length; i++) {
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const connection = state.scenarioMap.connections[i]!;
-    if (!state.scenarioMap.places.find((p) => p.location === connection.from))
-      return ctx.reject({
-        path: ['scenarioMap', 'connections', i.toString(), 'from'],
-        problem: `place ${connection.from} is not specified in map`,
-      });
-    if (!state.scenarioMap.places.find((p) => p.location === connection.to))
-      return ctx.reject({
-        path: ['scenarioMap', 'connections', i.toString(), 'to'],
-        problem: `place ${connection.to} is not specified in map`,
-      });
-    if (connection.from === connection.to)
-      return ctx.reject({
-        path: ['scenarioMap', 'connections', i.toString()],
-        problem: `Illegal connection to the same location`,
-      });
-  }
-
-  return true;
+}).narrow((state, ctx: Traversal) => {
+  return (
+    validateActs(state, ctx) &&
+    validateAgendas(state, ctx) &&
+    validateCurrentInvestigator(state, ctx) &&
+    validateInvestigators(state, ctx) &&
+    validatePlaces(state, ctx) &&
+    validateConnections(state, ctx)
+  );
 });
 
 export type GameState = typeof gameState.infer;
