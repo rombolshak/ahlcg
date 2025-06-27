9b24bc3acd4b3f94db01cc4e226c1a3cfbb7cd32
Roman Bolshakov <rombolshak@gmail.com>
2025-06-27 17:56:20 +0300

    feat: language selector component

--- frontend/src/app/pages/game-view/settings/settings.component.ts
+++ frontend/src/app/pages/game-view/settings/settings.component.ts
@@ -1,19 +1,51 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  effect,
+  inject,
+  linkedSignal,
+} from '@angular/core';
 import {
   provideUserPreferencesService,
   UserPreferences,
-} from '../../../shared/services/settings/user-preferences.service';
-import { SettingsService } from '../../../shared/services/settings/settings.service';
-import { TranslocoDirective } from '@jsverse/transloco';
+} from 'shared/services/settings/user-preferences.service';
+import { LangDefinition, TranslocoService } from '@jsverse/transloco';
+import { LanguageSelectorComponent } from './language-selector/language-selector.component';
+import { SettingsService } from 'shared/services/settings/settings.service';
+import { produce } from 'immer';
 
 @Component({
   selector: 'ah-settings',
-  imports: [TranslocoDirective],
+  imports: [LanguageSelectorComponent],
   templateUrl: './settings.component.html',
   styles: ``,
   providers: [provideUserPreferencesService()],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SettingsComponent {
-  readonly userPrefs = inject(SettingsService<UserPreferences>);
+  private readonly userPrefs = inject<SettingsService<UserPreferences>>(
+    SettingsService<UserPreferences>,
+  );
+  private readonly transloco = inject(TranslocoService);
+
+  protected readonly settings = linkedSignal(() => this.userPrefs.get()());
+  protected readonly availableLanguages = this.transloco.getAvailableLangs();
+
+  constructor() {
+    effect(() => {
+      this.transloco.setActiveLang(this.userPrefs.get()().lang);
+    });
+  }
+
+  protected setLang(lang: LangDefinition | string): void {
+    this.settings.update((current) =>
+      produce(current, (draft) => {
+        draft.lang = typeof lang === 'string' ? lang : lang.id;
+      }),
+    );
+  }
+
+  protected applySettings(): void {
+    this.userPrefs.set(this.settings());
+  }
 }
