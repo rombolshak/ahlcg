8bac543b55a06bd4b3c0f24f44152b7df1662085
Roman Bolshakov <rombolshak@gmail.com>
2025-07-07 21:12:19 +0300

    feat: default language provider (#180)

--- frontend/src/app/shared/services/settings/user-preferences.service.ts
+++ frontend/src/app/shared/services/settings/user-preferences.service.ts
@@ -5,6 +5,7 @@ import {
   STORAGE_KEY_SUFFIX,
 } from './settings.service';
 import { type } from 'arktype';
+import { TranslocoService } from '@jsverse/transloco';
 
 const userPreferences = type({
   lang: 'string',
@@ -17,9 +18,20 @@ export function provideUserPreferencesService(): Provider {
     { provide: STORAGE_KEY_SUFFIX, useValue: 'user-preferences' },
     {
       provide: DEFAULT_SETTINGS,
-      useValue: userPreferences.assert({
-        lang: 'fr',
-      }),
+      useFactory: (transloco: TranslocoService) => {
+        const available = transloco.getAvailableLangs();
+        const preferred = navigator.languages.find(
+          (lang) =>
+            available.findIndex((a) =>
+              typeof a === 'string' ? a === lang : a.id === lang,
+            ) !== -1,
+        );
+
+        return userPreferences.assert({
+          lang: preferred ?? transloco.getDefaultLang(),
+        });
+      },
+      deps: [TranslocoService],
     },
     {
       provide: SettingsService<UserPreferences>,
