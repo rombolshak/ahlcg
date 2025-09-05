ccea024f7471620afb297aaa06e055ae6d734140
Roman Bolshakov <rombolshak@gmail.com>
2025-09-05 12:55:50 +0300

    scenario name component tests and stories

--- frontend/src/app/pages/game-view/components/game-header/scenario-name/scenario-name.component.spec.ts
+++ frontend/src/app/pages/game-view/components/game-header/scenario-name/scenario-name.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ScenarioNameComponent } from './scenario-name.component';
+import { getTranslocoModule } from 'shared/domain/test/transloco.testing';
+import campaign from '../../../../../../../public/assets/i18n/campaigns/notz/en.json';
+import scenario from '../../../../../../../public/assets/i18n/campaigns/notz/mm/en.json';
+import { provideZonelessChangeDetection } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('ScenarioNameComponent', () => {
   let component: ScenarioNameComponent;
@@ -8,15 +13,25 @@ describe('ScenarioNameComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ScenarioNameComponent],
+      imports: [ScenarioNameComponent, getTranslocoModule()],
+      providers: [provideZonelessChangeDetection(), provideHttpClient()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ScenarioNameComponent);
     component = fixture.componentInstance;
+    fixture.componentRef.setInput('campaignId', 'notz');
+    fixture.componentRef.setInput('scenarioId', 'mm');
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display translated names', () => {
+    const nativeElement = fixture.debugElement.nativeElement as HTMLElement;
+
+    expect(nativeElement.textContent).toContain(campaign.name);
+    expect(nativeElement.textContent).toContain(scenario.name);
   });
 });
