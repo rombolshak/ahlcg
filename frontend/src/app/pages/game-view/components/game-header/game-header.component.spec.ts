d90ce2bd9713a7ac9e2845a267a0b15392e24ddc
Roman Bolshakov <rombolshak@gmail.com>
2025-09-08 18:01:37 +0300

    fix tests

--- frontend/src/app/pages/game-view/components/game-header/game-header.component.spec.ts
+++ frontend/src/app/pages/game-view/components/game-header/game-header.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GameHeaderComponent } from './game-header.component';
+import { provideZonelessChangeDetection } from '@angular/core';
+import { getTranslocoModule } from 'shared/domain/test/transloco.testing';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('GameHeaderComponent', () => {
   let component: GameHeaderComponent;
@@ -8,7 +11,8 @@ describe('GameHeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [GameHeaderComponent],
+      providers: [provideZonelessChangeDetection(), provideHttpClient()],
+      imports: [GameHeaderComponent, getTranslocoModule()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(GameHeaderComponent);
