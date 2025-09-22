40d1c48349854963df3b73e52a1d55e2068f5360
Roman Bolshakov <rombolshak@gmail.com>
2025-09-22 10:43:30 +0300

    assets placement rework

--- frontend/src/app/shared/services/images-url.service.ts
+++ frontend/src/app/shared/services/images-url.service.ts
@@ -3,78 +3,28 @@ import {
   AssetSlot,
   PlayerCardClassType,
   SkillType,
-} from 'shared/domain/entities/player-card.model';
-import { SetInfo } from 'shared/domain/entities/details/card-info.model';
+} from '../domain/entities/player-card.model';
+
+type CardTemplateType = 'asset' | 'event' | 'skill' | 'investigator';
+type SetName = string;
+type SetIndex = string;
+type SimpleOverlayType = 'clue' | 'resource' | 'doom' | 'health' | 'sanity';
+
+type ImageDescriptor =
+  | ['card-template', CardTemplateType, PlayerCardClassType]
+  | ['illustration' | 'investigator', SetName, SetIndex]
+  | ['set-icon', SetName]
+  | ['card-overlay', SimpleOverlayType]
+  | ['card-overlay', 'skill', 'box', PlayerCardClassType]
+  | ['card-overlay', 'skill', 'icon', SkillType]
+  | ['card-overlay', 'slot', AssetSlot]
+  | ['card-overlay', 'title' | 'subtitle', PlayerCardClassType];
 
 @Injectable({
   providedIn: 'root',
 })
 export class ImagesUrlService {
-  getTemplate(type: string, cardClass: string): string {
-    return `/assets/images/card-templates/${type}-${cardClass}.webp`;
-  }
-
-  getOverlay(info: Overlay | null): string {
-    if (!info) return '';
-    return this.getOverlayInternal(info.type, info.value);
-  }
-
-  getSimpleOverlay(type: string) {
-    return `/assets/images/card-overlays/${type}.png`;
-  }
-
-  getSetIcon(setId: string): string {
-    return `/assets/images/set-icons/${setId}.png`;
-  }
-
-  getIllustration(info: SetInfo): string {
-    return `/assets/images/illustrations/${info.set}/${info.index}.webp`;
-  }
-
-  getMiniIllustration(info: SetInfo): string {
-    return `/assets/images/illustrations/${info.set}/${info.index}-m.webp`;
-  }
-
-  getInvestigator(info: SetInfo): string {
-    return `/assets/images/investigators/${info.set}/${info.index}.webp`;
-  }
-
-  private getOverlayInternal(
-    type: OverlayType,
-    cardClass: PlayerCardClassType | AssetSlot | SkillType,
-  ): string {
-    return `/assets/images/card-overlays/${type}-${cardClass}.png`;
-  }
-}
-
-type OverlayType = 'skill-box' | 'skill-icon' | 'slot';
-
-type Overlay =
-  | {
-      type: 'skill-box';
-      value: PlayerCardClassType;
-    }
-  | {
-      type: 'skill-icon';
-      value: SkillType;
-    }
-  | {
-      type: 'slot';
-      value: AssetSlot;
-    };
-
-// eslint-disable-next-line @typescript-eslint/no-extraneous-class
-export class CreateOverlay {
-  static skillBox(value: PlayerCardClassType): Overlay {
-    return { type: 'skill-box', value: value };
-  }
-
-  static skillIcon(value: SkillType): Overlay {
-    return { type: 'skill-icon', value: value };
-  }
-
-  static cardSlot(value?: AssetSlot): Overlay | null {
-    if (!value) return null;
-    return { type: 'slot', value: value };
+  getUrl(desc: ImageDescriptor): string {
+    return `/assets/images/${desc.join('/')}.webp`;
   }
 }
