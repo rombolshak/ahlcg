5ff89e80318e8246ea65b0a5833f1c013608f020
Roman Bolshakov <rombolshak@gmail.com>
2025-10-10 14:00:40 +0300

    fix card-cost and vitals colors

--- frontend/src/app/shared/ui/components/cards/card-parts/card-cost/card-cost.component.ts
+++ frontend/src/app/shared/ui/components/cards/card-parts/card-cost/card-cost.component.ts
@@ -9,10 +9,10 @@ import { TextWithOverlayComponent } from '../../../text-with-overlay/text-with-o
   host: {
     class:
       'absolute' +
-      'group-data-[size=l]:left-[21px] group-data-[size=l]:top-3 group-data-[size=l]:w-9 group-data-[size=l]:text-3xl' +
-      'group-data-[size=m]:left-[14px] group-data-[size=m]:top-1 group-data-[size=m]:w-6 group-data-[size=m]:text-2xl' +
-      'group-data-[size=s]:left-[8px] group-data-[size=s]:top-0.5 group-data-[size=s]:w-4 group-data-[size=s]:text-sm' +
-      'text-center font-[Teutonic]',
+      ' group-data-[size=l]:left-[21px] group-data-[size=l]:top-3 group-data-[size=l]:w-9 group-data-[size=l]:text-3xl' +
+      ' group-data-[size=m]:left-[14px] group-data-[size=m]:top-1 group-data-[size=m]:w-6 group-data-[size=m]:text-2xl' +
+      ' group-data-[size=s]:left-[8px] group-data-[size=s]:top-0.5 group-data-[size=s]:w-4 group-data-[size=s]:text-sm' +
+      ' text-center font-[Teutonic]',
   },
 })
 export class CardCostComponent {
