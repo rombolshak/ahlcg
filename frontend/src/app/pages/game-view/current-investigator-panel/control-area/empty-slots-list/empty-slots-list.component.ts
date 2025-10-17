e7aef09d045c52be5c870dc97a85da611300bc62
Roman Bolshakov <rombolshak@gmail.com>
2025-10-17 21:40:32 +0300

    fix unused siglan

--- frontend/src/app/pages/game-view/current-investigator-panel/control-area/empty-slots-list/empty-slots-list.component.ts
+++ frontend/src/app/pages/game-view/current-investigator-panel/control-area/empty-slots-list/empty-slots-list.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { AssetSlot } from 'shared/domain/entities/player-card.model';
 import { EmptySlotComponent } from './empty-slot/empty-slot.component';
 
@@ -13,8 +13,4 @@ import { EmptySlotComponent } from './empty-slot/empty-slot.component';
 })
 export class EmptySlotsListComponent {
   readonly slots = input.required<AssetSlot[]>();
-
-  protected readonly emptyColumnsCount = computed(() =>
-    Math.max(this.slots().length, 6),
-  );
 }
