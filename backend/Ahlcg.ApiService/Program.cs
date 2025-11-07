982394b03da04417faf73154f08d9a088ddadb4d
Roman Bolshakov <rombolshak@gmail.com>
2025-11-07 22:29:35 +0300

    ahlcg-278 SignalR integration

--- backend/Ahlcg.ApiService/Program.cs
+++ backend/Ahlcg.ApiService/Program.cs
@@ -1,19 +1,27 @@
 using Ahlcg.ApiService;
 using Ahlcg.ServiceDefaults;
+using AspNetCore.SignalR.OpenTelemetry;
 using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.AddServiceDefaults();
-builder.AddNpgsqlDbContext<ApplicationDbContext>("ahlcg");
-builder.Services.AddProblemDetails();
-builder.Services.AddOpenApi();
-builder.Services.AddAuthorization();
-builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+builder
+    .AddServiceDefaults()
+    .AddNpgsqlDbContext<ApplicationDbContext>("ahlcg");
+
+builder.Services
+    .AddProblemDetails()
+    .AddOpenApi()
+    .AddAuthorization()
+    .AddSignalR().AddHubInstrumentation();
+
+builder.Services
+    .AddIdentityApiEndpoints<IdentityUser>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 app.UseExceptionHandler();
 app.MapDefaultEndpoints();
+app.MapHub<GameHub>("/game");
 
 if (app.Environment.IsDevelopment())
 {
