14fea83a90a9d2b681529fa0c292904716a4dbee
Roman Bolshakov <rombolshak@gmail.com>
2025-11-18 12:34:03 +0300

    feat: SignalR authentication (#322)
    * ahlcg-282 SignalR authentication
    
    * always complete parallel build
    
    * exclude generated files from analysis

--- backend/Ahlcg.ApiService/GameHub.cs
+++ backend/Ahlcg.ApiService/GameHub.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
 
 namespace Ahlcg.ApiService;
 
+[Authorize]
 public class GameHub : Hub
 {
     public async Task Ping() => await Clients.Caller.SendAsync("ping", DateTime.UtcNow);
