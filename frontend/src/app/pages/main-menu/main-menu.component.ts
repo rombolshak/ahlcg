836ff78823b9ea9b137eb82d62ed174c95d85cd7
Roman Bolshakov <rombolshak@gmail.com>
2025-12-05 20:11:57 +0300

    art button component

--- frontend/src/app/pages/main-menu/main-menu.component.ts
+++ frontend/src/app/pages/main-menu/main-menu.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ArtButtonComponent } from '@shared/components/art-button/art-button.component';
 import { ArtPanelComponent } from '@shared/components/art-panel/art-panel.component';
 
 @Component({
   selector: 'ah-main-menu',
-  imports: [ArtPanelComponent],
+  imports: [ArtPanelComponent, ArtButtonComponent],
   templateUrl: './main-menu.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
@@ -11,5 +12,8 @@ import { ArtPanelComponent } from '@shared/components/art-panel/art-panel.compon
       'h-screen w-screen block bg-[url("/assets/images/main-menu.webp")] bg-contain bg-center bg-no-repeat bg-black p-8',
   },
 })
-// eslint-disable-next-line @typescript-eslint/no-extraneous-class
-export class MainMenuComponent {}
+export class MainMenuComponent {
+  protected select() {
+    alert('qq');
+  }
+}
---
+++ frontend/src/app/shared/ui/components/art-button/art-button.stories.ts
@@ -0,0 +1,44 @@
+﻿import { ArtButtonComponent } from '@shared/components/art-button/art-button.component';
+import { Meta, StoryObj } from '@storybook/angular';
+
+const meta: Meta<ArtButtonComponent> = {
+  component: ArtButtonComponent,
+  parameters: {
+    layout: 'padded',
+  },
+};
+
+export default meta;
+type Story = StoryObj<ArtButtonComponent>;
+
+export const Normal: Story = {
+  render: (args) => ({
+    props: args,
+    template: `<button ah-art-button type="button">Default</button>`,
+  }),
+};
+
+export const Colored: Story = {
+  render: (args) => ({
+    props: args,
+    template: `
+      <button ah-art-button type="button" class="btn-primary">Primary</button>
+      <button ah-art-button type="button" class="btn-secondary">Secondary</button>
+      <button ah-art-button type="button" class="btn-accent">Accent</button>
+      <button ah-art-button type="button" class="btn-error">Error</button>
+    `,
+  }),
+};
+
+export const Sized: Story = {
+  render: (args) => ({
+    props: args,
+    template: `
+      <button ah-art-button type="button" class="btn-xs">Extra small</button>
+      <button ah-art-button type="button" class="btn-sm">Small</button>
+      <button ah-art-button type="button">Default</button>
+      <button ah-art-button type="button" class="btn-lg">Large</button>
+      <button ah-art-button type="button" class="btn-xl">Extra large</button>
+    `,
+  }),
+};
